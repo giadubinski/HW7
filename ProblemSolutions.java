f12df2a47954d18501c2d21a1c9bc4c55e8ef953
Gianna Dubinski <167164944+giadubinski@users.noreply.github.com>
2024-11-20 23:11:38 -0600

    Update ProblemSolutions.java

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -1,6 +1,6 @@
 /******************************************************************
  *
- *   YOUR NAME / SECTION NUMBER
+ *   Gianna Dubinski / COMP 272-001
  *
  *   This java file contains the problem solutions for the methods selectionSort,
  *   mergeSortDivisibleByKFirst, asteroidsDestroyed, and numRescueCanoes methods.
@@ -38,12 +38,25 @@ public class ProblemSolutions {
 
         for (int i = 0; i < n - 1; i++) {
 
-            // YOU CODE GOES HERE -- COMPLETE THE INNER LOOP OF THIS
-            // "SELECTION SORT" ALGORITHM.
-            // DO NOT FORGET TO ADD YOUR NAME / SECTION ABOVE
+            //Initializing range
+            int range = i;
 
+            //For loop to check the comparison based on ascending descending values
+            for (int j = i + 1; j < n; j++) {
+                boolean shouldSwap = ascending
+                        ? values[j] < values[range]
+                        : values[j] > values[range];
+
+                if (shouldSwap) {
+                    range = j;
+                }
+            }
+
+            //Initializing temp to swap the range of values
+            int temp = values[range];
+            values[range] = values[i];
+            values[i] = temp;
         }
-
     } // End class selectionSort
 
 
@@ -92,18 +105,40 @@ public class ProblemSolutions {
 
     private void mergeDivisbleByKFirst(int arr[], int k, int left, int mid, int right)
     {
-        // YOUR CODE GOES HERE, THIS METHOD IS NO MORE THAN THE STANDARD MERGE PORTION
-        // OF A MERGESORT, EXCEPT THE NUMBERS DIVISIBLE BY K MUST GO FIRST WITHIN THE
-        // SEQUENCE PER THE DISCUSSION IN THE PROLOGUE ABOVE.
-        //
-        // NOTE: YOU CAN PROGRAM THIS WITH A SPACE COMPLEXITY OF O(1) OR O(N LOG N).
-        // AGAIN, THIS IS REFERRING TO SPACE COMPLEXITY. O(1) IS IN-PLACE, O(N LOG N)
-        // ALLOCATES AUXILIARY DATA STRUCTURES (TEMPORARY ARRAYS). IT WILL BE EASIER
-        // TO CODE WITH A SPACE COMPLEXITY OF O(N LOG N), WHICH IS FINE FOR PURPOSES
-        // OF THIS PROGRAMMING EXERCISES.
+        //Initializing temp to new sum value
+        int[] temp = new int[right - left + 1];
+        int x = 0;
 
-        return;
+        //For loop to separate divisible and non-divisible numbers
+        for (int i = left; i <= right; i++) {
+            if (arr[i] % k == 0) {
+                temp[x++] = arr[i];
+            }
+        }
 
+        //Initializing temp to new sum value for non-divisible values
+        int[] nonDivisible = new int[right - left + 1];
+        int nonDivCount = 0;
+
+        //For loop to sort non-divisible numbers
+        for (int i = left; i <= right; i++) {
+            if (arr[i] % k != 0) {
+                nonDivisible[nonDivCount++] = arr[i];
+            }
+        }
+
+        //Array to sort non-divisible numbers fom the index
+        Arrays.sort(nonDivisible, 0, nonDivCount);
+
+        //For loop to add sorted non-divisible numbers
+        for (int i = 0; i < nonDivCount; i++) {
+            temp[x++] = nonDivisible[i];
+        }
+
+        //For loop to copy back to the original array
+        for (int i = 0; i < temp.length; i++) {
+            arr[left + i] = temp[i];
+        }
     }
 
 
@@ -154,9 +189,25 @@ public class ProblemSolutions {
 
     public static boolean asteroidsDestroyed(int mass, int[] asteroids) {
 
-        // YOUR CODE GOES HERE, CONSIDER USING ARRAYS.SORT()
+        // Sort people by weight in ascending order
+        Arrays.sort(asteroids);
 
-        return false;
+        //Use long to prevent integer overflow for large masses
+        long currentMass = mass;
+
+        //For loop to iterate through each asteroid
+        for (int asteroid : asteroids) {
+
+            //If statement to see if currentMass is less than the asteroid
+            if (currentMass < asteroid) {
+                return false;
+            }
+
+            //CurrentMass is what the asteroids destroyed
+            currentMass += asteroid;
+        }
+
+        return true;
 
     }
 
@@ -191,12 +242,38 @@ public class ProblemSolutions {
      */
 
     public static int numRescueSleds(int[] people, int limit) {
+        if (people.length == 0) return 0;
 
-        // YOUR CODE GOES HERE, CONSIDER USING ARRAYS.SORT
+        // Sort people by weight in ascending order
+        Arrays.sort(people);
 
-        return -1;
+        //Initializing to the left (lightest) and right (heaviest)
+        int left = 0, right = people.length - 1;
+        int sleds = 0;
 
+        //While loop to see if only one person left, they get their own sled
+        while (left <= right) {
+            if (left == right) {
+                sleds++;
+                break;
+            }
+
+            //If statement to see if to pair heaviest with the lightest riders
+            if (people[left] + people[right] <= limit) {
+                left++;
+                right--;
+                sleds++;
+            }
+
+            //Else if, a heavy person rides alone
+            else {
+                right--;
+                sleds++;
+            }
+        }
+
+        // Return total number of sleds used
+        return sleds;
     }
 
 } // End Class ProblemSolutions
-
